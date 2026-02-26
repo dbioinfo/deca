29d42bb11a42b87d864345c11fea9d4f8df2ff67
dylan-unlv <dylan.barth@unlv.edu>
2026-02-25 16:23:46 -0800

    initial cyano results

---
+++ src/deca_subset_cyano.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(phyloseq)
+
+setwd('~/WorkForaging/Academia/Nicole/deca/')
+
+ps <- readRDS("data/16Sdada2/phyloseq.filtered.tree.rds")
+tax <- data.frame(tax_table(ps))
+cyanoasv <- tax %>% filter(grepl("Cyanobac",Phylum)) %>% rownames()
+ps <- subset_taxa(ps, taxa_names(ps) %in% cyanoasv)
+ps <- subset_samples(ps, sample_sums(ps)>0)
+saveRDS(ps, "data/cyano/phyloseq.cyano.tree.rds")
+
+
